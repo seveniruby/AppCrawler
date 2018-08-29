c94a6decafa39449782e5e0ee96606003e17bc15
seveniruby <seveniruby@gmail.com>
2018-08-30 04:01:40 +0800

    2.4版本分支

--- src/main/scala/com/testerhome/appcrawler/AppCrawler.scala
+++ src/main/scala/com/testerhome/appcrawler/AppCrawler.scala
@@ -18,12 +18,12 @@ object AppCrawler extends CommonLog {
   val banner=
     """
       |----------------
-      |AppCrawler 2.4.0 [霍格沃兹测试学院特别纪念版]
-      |Appium 1.8.1 Java8 tested
+      |AppCrawler 2.4 [霍格沃兹测试学院特别纪念版]
+      |Appium 1.8.1 Java8 tested, author: seveniruby 思寒
       |app爬虫, 用于自动遍历测试. 支持Android和iOS, 支持真机和模拟器
       |项目地址: https://github.com/seveniruby/AppCrawler
       |移动测试技术交流: https://testerhome.com
-      |联络作者: seveniruby@testerhome.com (思寒)
+      |视频教程: https://ke.qq.com/course/271076
       |致谢: 晓光 泉龙 杨榕 恒温 mikezhou yaming116 沐木
       |
       |--------------------------------
