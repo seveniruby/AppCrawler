339010343f04308c7b19ab6e277b559fa41d7e1c
unknown <944075453@qq.com>
2019-01-15 14:45:04 +0800

    删除properties文件

--- src/main/java/com/testerhome/appcrawler/report/MvnReplace.java
+++ src/main/java/com/testerhome/appcrawler/report/MvnReplace.java
@@ -1,19 +1,24 @@
 package com.testerhome.appcrawler.report;
 
-import org.junit.jupiter.api.Test;
+import com.testerhome.appcrawler.AppCrawler;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.util.Properties;
+
 import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
-public class MvnRepalce {
-    @Test
+public class MvnReplace {
+
     public void runTest(){
+        Properties pro = new Properties();
+        pro.setProperty("allure.results.directory", AppCrawler.crawler().conf().resultDir() + "/allure-results");
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
                 .selectors(
                         selectPackage("com.testerhome.appcrawler.report"),
