24e7059207a7743260344d5fc89a6816523bda5d
seveniruby <seveniruby@gmail.com>
2019-05-03 20:53:42 +0800

    临时解决下adb driver的url问题

---
+++ src/main/scala/com/testerhome/appcrawler/plugin/junit5/AlllureTemplate.java
@@ -0,0 +1,27 @@
+package com.testerhome.appcrawler.plugin.junit5;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+public class AlllureTemplate {
+    @TestFactory
+    Collection<DynamicTest> AllTestCases() {
+        ArrayList<DynamicTest> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(dynamicTest(String.format("testcase %s ", i), () -> {
+                        assertTrue(false);
+                    }
+            ));
+        }
+
+        return arrayList;
+    }
+}
---
+++ src/main/scala/com/testerhome/appcrawler/plugin/junit5/JUnit5Runtime.scala
@@ -0,0 +1,30 @@
+package com.testerhome.appcrawler.plugin.junit5
+
+import com.testerhome.appcrawler.data.AbstractElementStore
+import com.testerhome.appcrawler.plugin.scalatest.SuiteToClass
+import com.testerhome.appcrawler.{Report, ReportFactory, TData}
+import org.scalatest.tools.Runner
+
+import scala.collection.JavaConversions._
+import scala.io.Source
+
+/**
+  * Created by seveniruby on 16/8/15.
+  */
+class JUnit5Runtime extends Report {
+
+  override def genTestCase(resultDir: String): Unit = {
+    //todo:
+  }
+
+
+  //todo: 用junit+allure代替
+  override def runTestCase(namespace: String=""): Unit = {
+    //todo:
+  }
+
+  override def changeTitle(title:String): Unit ={
+    //todo:
+  }
+
+}
