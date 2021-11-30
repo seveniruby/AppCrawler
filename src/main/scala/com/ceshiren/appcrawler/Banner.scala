17e938199aa99da02a3f7c857a0f7ed4429fef12
天马 <yangchenhui@ceshiren.com>
2021-11-30 15:16:18 +0800

    适配项目拆分

---
+++ src/main/scala/com/ceshiren/appcrawler/Banner.scala
@@ -0,0 +1,13 @@
+package com.ceshiren.appcrawler
+
+object Banner {
+  val banner =
+    """
+      |-------------------------------------------------
+      |appcrawler v2.7.2 全平台自动遍历测试工具
+      |Q&A: https://ceshiren.com/c/opensource/appcrawler
+      |author: 思寒 seveniruby@霍格沃兹测试开发学社
+      |-------------------------------------------------
+      |""".stripMargin
+
+}
